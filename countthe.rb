fd02209f1a34cc2797070a3a41dd76829fa73b4e
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 23:34:18 +0000

    initial commit

---
+++ countthe.rb
@@ -0,0 +1,10 @@
+sentences = [
+  "the dog, the cat, the zebra, the giraffe",
+  "the, the code, and the developer",
+  "then the- their"
+]
+sentence = sentences.sample
+# write your program below
+#   "'the' appeared X times"
+
+
