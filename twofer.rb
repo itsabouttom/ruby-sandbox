ae8c2343c7311074038f368a44eb7d1361079cf2
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 20:11:38 +0000

    added capitalize if statement

--- twofer.rb
+++ twofer.rb
@@ -2,3 +2,8 @@ name = ["raghu", "Bob", ""].sample
 # write your program below
 if name == ""
   print "One for you, and one for me."
+else 
+  name = name.downcase
+  name = name.capitalize
+  print "One for #{name}, and one for me."
+end
