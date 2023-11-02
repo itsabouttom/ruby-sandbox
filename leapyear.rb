1e25c61b2105eed26d2268a47a0374e69ccb97b5
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 20:43:51 +0000

    reordered if statements

--- leapyear.rb
+++ leapyear.rb
@@ -7,14 +7,14 @@ year = years.sample
 # write your program below
 
 if year % 4 == 0 
-  if year % 100 == 0
+  if year % 100 != 0
+    print "#{year} is a leap year!"
+  else
     if year % 400 == 0
       print "#{year} is a leap year!"
     else
-      "#{year} is not a leap year."
+      print "#{year} is not a leap year."
     end
-  else
-    print "#{year} is a leap year!"
   end
 else
   print "#{year} is not a leap year."
