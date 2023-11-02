9b391fafe882507e7df0cc5e79e6cccbce628686
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 23:14:03 +0000

    initial commit

---
+++ charactertypes.rb
@@ -0,0 +1,17 @@
+strings = [
+  "here 12 plus 25",
+  "puzzle number 04 ",
+  " "
+]
+string = strings.sample
+pp string
+# write your program below
+=begin 
+Takes a String
+Counts the total number of letters in the given String
+Counts the total number of spaces in the given String
+Counts the total numbers of digits in the given String
+and prints the information out
+=end
+
+
