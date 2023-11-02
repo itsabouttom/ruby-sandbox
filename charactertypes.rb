4a9048285a2b93b7cda890ce936d0d7d14fdbd8f
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 23:31:15 +0000

    updated gsub

--- charactertypes.rb
+++ charactertypes.rb
@@ -14,4 +14,18 @@ Counts the total numbers of digits in the given String
 and prints the information out
 =end
 
+letters = string.gsub(/[^a-z]/i, "")
+spaces = string.gsub(/[^\s+]/, "")
+digits = string.gsub(/[^0-9]/, "")
 
+letter_array = letters.split("")
+spaces_array = spaces.split("")
+digits_array = digits.split("")
+
+letter_count = letter_array.count
+spaces_count = spaces_array.count
+digits_count = digits_array.count
+
+print "Number of letters in the string is: #{letter_count}"
+print "Number of spaces in the string is: #{spaces_count}"
+print "Number of digits in the string is: #{digits_count}"
