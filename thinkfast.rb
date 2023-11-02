0afc215e2c51865cf31a8942b7a575083bc78324
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 19:22:41 +0000

    removed .class from nil statement

--- thinkfast.rb
+++ thinkfast.rb
@@ -16,6 +16,7 @@ some_random_input = unpredictable_inputs.sample
 
 if some_random_input.class == String
     print some_random_input.downcase
+    print some_random_input
 
 elsif some_random_input.class == Time
     year_input = some_random_input.year
@@ -25,44 +26,60 @@ elsif some_random_input.class == Time
     weekday = date.wday 
       if weekday == 0
         print "sunday"
+        print some_random_input
       elsif weekday == 1
         print "monday"
+        print some_random_input
       elsif weekday == 2
         print "tuesday"
+        print some_random_input
       elsif weekday == 3
         print "wednesday"
+        print some_random_input
       elsif weekday == 4
         print "thursday"
+        print some_random_input
       elsif weekday == 5 
         print "friday"
+        print some_random_input
       elsif weekday == 6
         print "saturday"
+        print some_random_input
       else
         print "error in Time statement"
+        print some_random_input
       end
     
 elsif some_random_input.class == Integer
       if some_random_input.odd? == true
         print "#{some_random_input} is even"
+        print some_random_input
       elsif some_random_input.even? == true
         print "#{some_random_input} is odd"
+        print some_random_input
       else
         print "error in Integer statement"
+        print some_random_input
       end
     
 elsif some_random_input.class == Symbol
       print some_random_input.downcase
+      print some_random_input
     
-elsif some_random_input.class == nil
+elsif some_random_input == nil
       print "no object provided"
+      print some_random_input
 
 elsif some_random_input == true
       print "you may pass"
+      print some_random_input
 elsif some_random_input == false
       print "you may not pass"
+      print some_random_input
 
 elsif some_random_input.class == Hash
       print some_random_input.keys
+      print some_random_input
     
 else 
     print some_random_input
