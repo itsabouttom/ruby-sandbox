459459d5422938e0e9b45fac3d184bdf61df50cf
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 18:17:27 +0000

    think fast initial commit

---
+++ thinkfast.rb
@@ -0,0 +1,14 @@
+unpredictable_inputs = [
+  "Hello!",
+  Time.now,
+  rand(100),
+  :GOODBYE,
+  nil,
+  true,
+  false,
+  { :city => "Chicago", :state => "IL", :zip => 60654 }
+]
+
+some_random_input = unpredictable_inputs.sample
+# write your program below
+
