bb62cfeb474167cb578cad56598ffb878977598f
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 20:17:24 +0000

    removed and from print statement

--- twofer.rb
+++ twofer.rb
@@ -1,9 +1,9 @@
 name = ["raghu", "Bob", ""].sample
 # write your program below
 if name == ""
-  print "One for you, and one for me."
+  print "One for you, one for me."
 else 
   name = name.downcase
   name = name.capitalize
-  print "One for " + name + ", and one for me"
+  print "One for " + name + ", one for me"
 end
