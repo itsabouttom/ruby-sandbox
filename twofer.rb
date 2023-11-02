1872e132588fca8c27e60f56019b90e4b5ce1393
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 20:17:51 +0000

    removed period from noname print stament

--- twofer.rb
+++ twofer.rb
@@ -1,7 +1,7 @@
 name = ["raghu", "Bob", ""].sample
 # write your program below
 if name == ""
-  print "One for you, one for me."
+  print "One for you, one for me"
 else 
   name = name.downcase
   name = name.capitalize
