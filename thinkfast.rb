5d134b8511cd5a9aa4581cc344b24c3cff646caf
itsabouttom <147127136+itsabouttom@users.noreply.github.com>
2023-11-02 18:45:43 +0000

    completed time if statement

--- thinkfast.rb
+++ thinkfast.rb
@@ -13,10 +13,35 @@ unpredictable_inputs = [
 
 some_random_input = unpredictable_inputs.sample
 # write your program below
+
+
   if some_random_input.class == String
     print some_random_input.downcase
+
   elsif some_random_input.class == Time
-    some_random_input.
+    year_input = some_random_input.year
+    month_input = some_random_input.month
+    day_input = some_random_input.day 
+    date = Date.new(year_input, month_input, day_input) 
+    weekday = date.wday 
+      if weekday == 0
+        print "sunday"
+      elsif weekday == 1
+        print "monday"
+      elsif weekday == 2
+        print "tuesday"
+      elsif weekday == 3
+        print "wednesday"
+      elsif weekday == 4
+        print "thursday"
+      elsif weekday == 5 
+        print "friday"
+      elsif weekday == 6
+        print "saturday"
+      else
+        print "error in day of week calculation"
+      end
     
+    elsif 
     
   end
